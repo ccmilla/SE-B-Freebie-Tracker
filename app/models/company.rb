d3d209a21f36bf0ed53a3b076e264856ea557ff4
Cecilia Milla <cmilla1020@gmail.com>
2022-12-23 12:51:27 -0500

    adding class solutions

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,12 @@
 class Company < ActiveRecord::Base
-
+   has_many :freebies
+   has_many :devs, through: :freebies
+   def give_freebie(dev, item_name, value)
+     Freebie.create(item_name: item_name, item_value: value, company_id: self.id, dev_id: dev.id)
+   end
+   def oldest_company
+     #Company.where("founding_year = ?", Company.minimum(:founding_year))
+     #self.all.order(:founding_year).first
+     Company.all.min_by { |company| company.founding_year }
+   end
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,19 @@
 class Dev < ActiveRecord::Base
+   has_many :freebies
+   has_many :companies, through: :freebies
 
+   def received_one?(item_name)
+    # 1. One way
+    #   self.freebies.any? do |freebie|
+    #     freebie.item_name == item_name
+    #   end
+    # 2. Another way credit: Caitlin
+    #  freebies.exists? item_name: item_name
+    # 3. Third way credit: Caitlin
+    freebies.map {|free| free.item_name}.include? item_name
+   end
+   def give_away(freebie, dev)
+    #  freebie.update(dev: dev) unless freebie.dev !=self
+      freebie.dev == self ? freebie.update(dev: dev) : "not yours!" # credit: Ryan
+   end
 end
